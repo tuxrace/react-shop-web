885d80d02bc9f4f29266b4820b2fe38a1aa8d8af
Gerard Banasig <tuxrace@yahoo.com>
2020-05-22 13:13:47 +0800

    Updates

---
+++ precache-manifest.36498e400dae7118e0cde5a424a1c230.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "67e2cf0dead3e286df4552137489c6d8",
+    "url": "/react-shop-web/index.html"
+  },
+  {
+    "revision": "ee93ef48b1f7f93375ce",
+    "url": "/react-shop-web/static/css/main.0fd90cc1.chunk.css"
+  },
+  {
+    "revision": "0009a36e332fc6c18dfd",
+    "url": "/react-shop-web/static/js/2.5bb42891.chunk.js"
+  },
+  {
+    "revision": "d466ab9648f013dea081956fc7c904f3",
+    "url": "/react-shop-web/static/js/2.5bb42891.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ee93ef48b1f7f93375ce",
+    "url": "/react-shop-web/static/js/main.724a8c91.chunk.js"
+  },
+  {
+    "revision": "91b529bd55675c89cbd1",
+    "url": "/react-shop-web/static/js/runtime-main.aabed74d.js"
+  }
+]);
